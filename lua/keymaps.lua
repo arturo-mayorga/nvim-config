51e92052f48b49875048655be0af83dc6598f567
Arturo Mayorga <arturo@mayorga.io>
2025-07-27 15:54:32 -0400

    ...

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,6 +5,9 @@ local map = vim.keymap.set
 -- Fuzzy file finder
 map("n", "<C-p>", require("telescope.builtin").find_files, { desc = "Telescope Find Files" })
 
+-- LSP go to definition
+map('n', 'gd', vim.lsp.buf.definition, { noremap = true, silent = true })
+
 -- Add more here later as needed
 -- e.g. map("n", "<leader>cc", ":CopilotChatToggle<CR>", { desc = "Toggle Copilot Chat" })
 -- map("n", "<leader>cc", ":CopilotChatToggle<CR>", { desc = "Copilot Chat Toggle" })
