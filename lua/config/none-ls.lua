9938a67f3eaaad9b90ebbf1b10f03721a6cf307c
Arturo Mayorga <arturo@mayorga.io>
2025-08-14 19:50:11 -0400

    lint config

--- lua/config/none-ls.lua
+++ lua/config/none-ls.lua
@@ -3,7 +3,12 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.diagnostics.pylint,
-    null_ls.builtins.diagnostics.markdownlint,   -- needs `markdownlint-cli`
-    null_ls.builtins.formatting.prettier,        -- prettier also formats Markdown
+    null_ls.builtins.diagnostics.markdownlint.with({
+      extra_args = {
+        "--config",
+        vim.fn.stdpath("config") .. "/.markdownlint.json",  -- your file’s path
+      },
+    }),
+    null_ls.builtins.formatting.prettier,
   },
 })
