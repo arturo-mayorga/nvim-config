c8c71ef05e002649922ed222b3d9e71c8c067817
Arturo Mayorga <arturo@mayorga.io>
2025-08-17 13:00:07 -0400

    markdown fixes for windows -- start

--- lua/config/markdown.lua
+++ lua/config/markdown.lua
@@ -1,3 +1,49 @@
+-- lua/config/markdown.lua
+local fml = require("follow-md-links")
+
+-- Patch plugin for Windows if not already patched
+if not fml._patched_for_windows then
+  local original = fml.follow_link
+
+  fml.follow_link = function()
+    local link = fml.get_link_under_cursor()
+    if not link or link == "" then return end
+
+    -- Let plugin handle web URLs or markdown heading anchors
+    if link:match("^https?://") or link:match("^#") then
+      return original()
+    end
+
+    -- Try to open local file paths (fix for Windows)
+    local file, anchor = link:match("^(.-)#(.+)$")
+    file = file or link
+
+    -- If not absolute, convert to full path
+    if not file:match("^/") and not file:match("^%a:[/\\]") and not file:match("^~") then
+      local base = vim.fn.expand("%:p:h")
+      file = vim.fn.fnamemodify(base .. "/" .. file, ":p")
+    end
+
+    local path, lnum = file:match("^(.-):(%d+)$")
+    if path and lnum then
+      vim.cmd(("edit +%s %s"):format(lnum, vim.fn.fnameescape(path)))
+    else
+      vim.cmd(("edit %s"):format(vim.fn.fnameescape(file)))
+    end
+
+    -- Jump to heading if present
+    if anchor and anchor ~= "" then
+      local pat = "\\v^#+\\s+" .. vim.fn.escape(anchor, "\\/.*$^~[]")
+      if vim.fn.search(pat, "w") == 0 then
+        vim.fn.search(vim.fn.escape(anchor, "\\/.*$^~[]"), "w")
+      end
+    end
+  end
+
+  fml._patched_for_windows = true
+end
+
+-- Markdown-specific keymaps (on FileType)
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
   callback = function(ev)
